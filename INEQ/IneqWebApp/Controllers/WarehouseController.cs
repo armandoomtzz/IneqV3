aeb881838f99fdb7af0f9c33e7c5658530ec2bb7
irma__000 <irma__000@IRMA>
2016-09-25 21:30:00 -0500

    Se completo el Caso de uso 7

---
+++ INEQ/IneqWebApp/Controllers/WarehouseController.cs
@@ -0,0 +1,44 @@
+﻿using IneqApi.Models;
+using IneqWebApp.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IneqWebApp.Controllers
+{
+    public class WarehouseController : Controller
+    {
+        // GET: Warehouse
+        public ActionResult Index()
+        {
+            List<Warehouse> model = new List<Warehouse>();
+            model = new WarehouseData().Get("", true);
+            return View(model);   
+        }
+
+        // GET: Warehouse/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Warehouse/Create
+        [HttpPost]
+        public ActionResult Create(int id, string description,string Is,string responsable,bool active)
+        {
+            WarehouseData wh = new WarehouseData();
+            wh.Save( id, description,  Is, responsable,  active);
+          return  RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public ActionResult Update(int id, string description, string Is, string responsable, bool active)
+        {
+            WarehouseData wh = new WarehouseData();
+            wh.Save(id, description, Is, responsable, active);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ INEQ/IneqWebApp/Util/Constant.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IneqWebApp.Util
+{
+    public class Constant
+    {
+        public static string THROW_EX_BO_UNAUTHORIZED = "Unauthoriezd";
+        public static string THROW_EX_BO_UNAUTHORIZED_ERROR = "Unauthoriezd Error";
+    }
+}
